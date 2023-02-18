f45bb7ea834b12491c9fa73190f3ef229c93d3a3
quangndgcc200030 <91590378+quangndgcc200030@users.noreply.github.com>
2023-02-18 22:38:41 +0700

    Register and Login

--- FPTBookWebClient/Areas/Identity/Pages/Account/Manage/Index.cshtml.cs
+++ FPTBookWebClient/Areas/Identity/Pages/Account/Manage/Index.cshtml.cs
@@ -7,6 +7,7 @@ using System.ComponentModel.DataAnnotations;
 using System.Text.Encodings.Web;
 using System.Threading.Tasks;
 using BusinessObjects;
+using FPTBookWebClient.Services;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -17,13 +18,16 @@ namespace FPTBookWebClient.Areas.Identity.Pages.Account.Manage
     {
         private readonly UserManager<AppUser> _userManager;
         private readonly SignInManager<AppUser> _signInManager;
+        private readonly IFileService _fileService;
 
         public IndexModel(
             UserManager<AppUser> userManager,
-            SignInManager<AppUser> signInManager)
+            SignInManager<AppUser> signInManager,
+            IFileService fileService)
         {
             _userManager = userManager;
             _signInManager = signInManager;
+            _fileService = fileService;
         }
 
         /// <summary>
@@ -59,6 +63,29 @@ namespace FPTBookWebClient.Areas.Identity.Pages.Account.Manage
             [Phone]
             [Display(Name = "Phone number")]
             public string PhoneNumber { get; set; }
+
+            [Required(ErrorMessage = "First name can not empty!")]
+            [Display(Name = "First name")]
+            public string FirstName { get; set; }
+
+            [Required(ErrorMessage = "Last name can not empty!")]
+            [Display(Name = "Last name")]
+            public string LastName { get; set; }
+
+            [Required(ErrorMessage = "Please choose your gender!")]
+            [Display(Name = "Gender")]
+            public bool Gender { get; set; }
+
+            [Required(ErrorMessage = "Please choose your birthday!")]
+            [Display(Name = "Birthday")]
+            public DateTime Birthday { get; set; }
+
+            [Required(ErrorMessage = "Address can not empty!")]
+            [Display(Name = "Address")]
+            public string Address { get; set; }
+
+            public string ProfilePicture { get; set; }
+            public IFormFile ImageFile { get; set; }
         }
 
         private async Task LoadAsync(AppUser user)
@@ -70,7 +97,13 @@ namespace FPTBookWebClient.Areas.Identity.Pages.Account.Manage
 
             Input = new InputModel
             {
-                PhoneNumber = phoneNumber
+                PhoneNumber = phoneNumber,
+                FirstName = user.FirstName,
+                LastName = user.LastName,
+                Gender = user.Gender.Value,
+                Birthday = user.Birthday.Value,
+                Address = user.Address,
+                ProfilePicture = user.ProfilePicture,
             };
         }
 
@@ -108,6 +141,48 @@ namespace FPTBookWebClient.Areas.Identity.Pages.Account.Manage
                 {
                     StatusMessage = "Unexpected error when trying to set phone number.";
                     return RedirectToPage();
+                }
+            }
+
+            if (Input.FirstName != user.FirstName)
+            {
+                user.FirstName = Input.FirstName;
+                await _userManager.UpdateAsync(user);
+            }
+
+            if (Input.LastName != user.LastName)
+            {
+                user.LastName = Input.LastName;
+                await _userManager.UpdateAsync(user);
+            }
+
+            if (Input.Gender != user.Gender)
+            {
+                user.Gender = Input.Gender;
+                await _userManager.UpdateAsync(user);
+            }
+
+            if (Input.Birthday != user.Birthday)
+            {
+                user.Birthday = Input.Birthday;
+                await _userManager.UpdateAsync(user);
+            }
+
+            if (Input.Address != user.Address)
+            {
+                user.Address = Input.Address;
+                await _userManager.UpdateAsync(user);
+            }
+
+            if (Input.ImageFile != null)
+            {
+                var result = _fileService.SaveImage(Input.ImageFile);
+                if (result.Item1 == 1)
+                {
+                    var oldImage = user.ProfilePicture;
+                    user.ProfilePicture = result.Item2;
+                    await _userManager.UpdateAsync(user);
+                    var deleteResult = _fileService.DeleteImage(oldImage);
                 }
             }
 
---
+++ FPTBookWebClient/Services/IFileService.cs
@@ -0,0 +1,8 @@
+﻿namespace FPTBookWebClient.Services
+{
+    public interface IFileService
+    {
+        Tuple<int, string> SaveImage(IFormFile imageFile);
+        public bool DeleteImage(string imageFileName);
+    }
+}
