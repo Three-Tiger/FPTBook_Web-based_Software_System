8b318f2be741ad5004fc0dbe3bb99e5b7807caca
DuongntGCC200026 <duongntgcc200026@fpt.edu.vn>
2023-02-21 02:08:02 +0700

    function of book

--- FPTBookWebClient/Areas/Owners/Controllers/BookController.cs
+++ FPTBookWebClient/Areas/Owners/Controllers/BookController.cs
@@ -1,87 +1,88 @@
-﻿using FPTBookWebClient.Constants;
+﻿using BusinessObjects;
+using FPTBookWebClient.Constants;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Net.Http.Headers;
+using System.Text.Json;
 
 namespace FPTBookWebClient.Areas.Owners.Controllers
 {
-    public class BookController : Controller
+	[Authorize(Roles = "Owner")]
+	[Area("Owners")]
+	public class BookController : Controller
     {
-        [Authorize(Roles = "Owner")]
-        [Area("Owners")]
-        // GET: BookController
-        public ActionResult Index()
-        {
-            return View();
-        }
+		private readonly HttpClient client = null;
+		private string api;
+		public BookController()
+		{
+			client = new HttpClient();
+			var contentType = new MediaTypeWithQualityHeaderValue("application/json");
+			client.DefaultRequestHeaders.Accept.Add(contentType);
+			this.api = "https://localhost:7076/api/Books";
+		}
+		public async Task<IActionResult> Index()
+		{
+			HttpResponseMessage httpResponse = await client.GetAsync(api);
+			string data = await httpResponse.Content.ReadAsStringAsync();
+			var options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+			List<Book> books = JsonSerializer.Deserialize<List<Book>>(data, options);
+			return View(books);
+		}
+		public IActionResult Create()
+		{
+			return View();
+		}
 
-        // GET: BookController/Details/5
-        public ActionResult Details(int id)
-        {
-            return View();
-        }
+		[HttpPost]
+		public async Task<IActionResult> Create(Book book)
+		{
+			if (ModelState.IsValid)
+			{
+				string data = JsonSerializer.Serialize(book);
+				var content = new StringContent(data, System.Text.Encoding.UTF8, "application/json");
+				HttpResponseMessage response = await client.PostAsync(api, content);
+				if (response.IsSuccessStatusCode)
+				{
+					return RedirectToAction("Index");
+				}
+			}
+			return View(book);
+		}
+		public async Task<IActionResult> Update(int id)
+		{
+			HttpResponseMessage reponse = await client.GetAsync(api + "/" + id);
+			if (reponse.IsSuccessStatusCode)
+			{
+				var data = reponse.Content.ReadAsStringAsync().Result;
+				var options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+				var obj = JsonSerializer.Deserialize<Book>(data, options);
+				return View(obj);
+			}
+			return NotFound();
+		}
 
-        // GET: BookController/Create
-        public ActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: BookController/Create
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: BookController/Edit/5
-        public ActionResult Edit(int id)
-        {
-            return View();
-        }
-
-        // POST: BookController/Edit/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: BookController/Delete/5
-        public ActionResult Delete(int id)
-        {
-            return View();
-        }
-
-        // POST: BookController/Delete/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
-    }
+		[HttpPost]
+		public async Task<IActionResult> Update(int id, Book book)
+		{
+			book.BookId = id;
+			string data = JsonSerializer.Serialize<Book>(book);
+			var content = new StringContent(data, System.Text.Encoding.UTF8, "application/json");
+			HttpResponseMessage response = await client.PutAsync(api + "/" + id, content);
+			if (response.IsSuccessStatusCode)
+			{
+				return RedirectToAction("Index");
+			}
+			return View(book);
+		}
+		public async Task<IActionResult> Delete(int id)
+		{
+			HttpResponseMessage reponse = await client.DeleteAsync(api + "/" + id);
+			if (reponse.IsSuccessStatusCode)
+			{
+				return RedirectToAction("Index");
+			}
+			return NotFound();
+		}
+	}
 }
