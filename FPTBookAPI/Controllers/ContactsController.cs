c4a26b55ef1896d02bb29c6f4555eb8d2e49cef1
quangndgcc200030 <91590378+quangndgcc200030@users.noreply.github.com>
2023-02-20 22:20:42 +0700

    fix push api

---
+++ FPTBookAPI/Controllers/ContactsController.cs
@@ -0,0 +1,65 @@
+﻿using BusinessObjects;
+using Microsoft.AspNetCore.Mvc;
+using Repositories;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace FPTBookAPI.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class ContactsController : ControllerBase
+	{
+		private IContactRepository repository = new ContactRepository();
+
+		// GET: api/<ContactsController>
+		[HttpGet]
+		public ActionResult<IEnumerable<Contact>> Get()
+		{
+			return repository.GetContacts();
+		}
+
+		// GET api/<ContactsController>/5
+		[HttpGet("{id}")]
+		public ActionResult<Contact> Get(int id)
+		{
+			return repository.GetContactById(id);
+		}
+
+		// POST api/<ContactsController>
+		[HttpPost]
+		public IActionResult Post([FromBody] Contact obj)
+		{
+			repository.SaveContact(obj);
+			return NoContent();
+		}
+
+		// PUT api/<ContactsController>/5
+		[HttpPut("{id}")]
+		public IActionResult Put(int id, [FromBody] Contact obj)
+		{
+			var contact = repository.GetContactById(id);
+			if (contact == null)
+			{
+				return NotFound();
+			}
+			obj.ContactDate = contact.ContactDate;
+			obj.ReplyDate = DateTime.Now;
+			repository.UpdateContact(obj);
+			return NoContent();
+		}
+
+		// DELETE api/<ContactsController>/5
+		[HttpDelete("{id}")]
+		public IActionResult Delete(int id)
+		{
+			var contact = repository.GetContactById(id);
+			if (contact == null)
+			{
+				return NotFound();
+			}
+			repository.DeleteContact(contact);
+			return NoContent();
+		}
+	}
+}
---
+++ Repositories/IContactRepository.cs
@@ -0,0 +1,18 @@
+﻿using BusinessObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositories
+{
+	public interface IContactRepository
+	{
+		List<Contact> GetContacts();
+		Contact GetContactById(int id);
+		void SaveContact(Contact contact);
+		void UpdateContact(Contact contact);
+		void DeleteContact(Contact contact);
+	}
+}
